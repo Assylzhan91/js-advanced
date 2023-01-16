90a4079e83b177d44eac447eef438afe79347893
Assylzhan <a.baymuratov@kazdream.kz>
2023-01-16 18:49:20 +0600

    add eslint & refactor getWords func

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { getWords, wordsCount } from './hw'
 
 window.addEventListener('load', function (){
-    let str = '  Всем привет! Ура ура!';
+    //let str = '  Всем привет! Ура ура!';
+    let str = 'aa';
     let count = wordsCount(str)
-        console.log(count)
 
     let a =  getWords(str)
 
     for (const aElement of a) {
         console.log(aElement)
     }
+
 })
